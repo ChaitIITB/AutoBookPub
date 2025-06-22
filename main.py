2fe41df8fcb8b8d6125e1709dad7ea76c405b4d6
ChaitIITB <23b2712@iitb.ac.in>
2025-06-22 23:41:43 +0530

    Edited the file

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 from dotenv import load_dotenv
 import chromadb
-
 load_dotenv()
-
 import getpass
 import os
-
-if "GOOGLE_API_KEY" not in os.environ:
-    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Google AI API key: ")
-
-
 from langchain_google_genai import ChatGoogleGenerativeAI
-
 from playwright.sync_api import sync_playwright
+import base64
+from langchain.schema import HumanMessage
 
 with sync_playwright() as p:
     for browser_type in [p.chromium, p.firefox, p.webkit]:
         browser = browser_type.launch()
         page = browser.new_page()
-        page.goto('https://en.wikisource.org/wiki/The_Gates_of_Morning/Book_1/Chapter_1.')
+        page.goto('https://en.wikisource.org/wiki/The_Gates_of_Morning/Book_1/Chapter_1')
         page.screenshot(path=f'example-{browser_type.name}.png')
         browser.close()
         print(f"Screenshot taken with {browser_type.name} browser.")
 
+
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Google AI API key: ")
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
@@ -33,15 +30,18 @@ llm = ChatGoogleGenerativeAI(
     # other params...
 )
 
-messages = [
-    (
-        "system",
-        "You are a helpful assistant that translates English to French. Translate the user sentence.",
-    ),
-    ("human", "I love programming."),
-]
-ai_msg = llm.invoke(messages)
-print(ai_msg.content)
+image_file_path = "example-chromium.png"
 
+with open(image_file_path, "rb") as image_file:
+    encoded_image = base64.b64encode(image_file.read()).decode("utf-8")
+
+message_local = HumanMessage(
+    content=[
+        {"type": "text", "text": "You are a book AI book writer, you don't write original content, but write a spin on the existing content, this is done using the image of a book page that is provided to you, in the response, you will write a new chapter based on the image provided."},
+        {"type": "image_url", "image_url": f"data:image/png;base64,{encoded_image}"},
+    ]
+)
+result_local = llm.invoke([message_local])
+print(f"Response for local image: {result_local.content}")
 
 
